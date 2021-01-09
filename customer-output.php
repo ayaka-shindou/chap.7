13c2806494a7e9345230307db5c429ee7234271b
ayaka-shindou <ayaka120shindou@gmail.com>
2021-01-09 19:26:00 +0900

    initial commit

---
+++ customer-output.php
@@ -0,0 +1,40 @@
+<?php session_start(); ?>
+<?php require '../header.php'; ?>
+<?php require 'menu.php'; ?>
+<?php
+
+$pdo=new PDO('mysql:host=localhost;dbname=snay_shop;charset=utf8',
+'shindou_ayaka', 'Asdf3333-');
+  
+if (isset($_SESSION['customer'])) {
+	$id=$_SESSION['customer']['id'];
+	$sql=$pdo->prepare('select * from customer where id!=? and login=?');
+	$sql->execute([$id, $_REQUEST['login']]);
+} else {
+	$sql=$pdo->prepare('select * from customer where login=?');
+	$sql->execute([$_REQUEST['login']]);
+}
+if (empty($sql->fetchAll())) {
+	if (isset($_SESSION['customer'])) {
+		$sql=$pdo->prepare('update customer set name=?, address=?, '.
+			'login=?, password=?, mailaddress?  where id=?');
+		$sql->execute([
+			$_REQUEST['name'], $_REQUEST['address'], 
+			$_REQUEST['login'], $_REQUEST['password'], $_REQUEST['mikaddress'],  $id]);
+		$_SESSION['customer']=[
+			'id'=>$id, 'name'=>$_REQUEST['name'], 
+			'address'=>$_REQUEST['address'], 'login'=>$_REQUEST['login'], 
+			'password'=>$_REQUEST['password'], 'mailaddress'=>$_REQUEST['mailaddress']];
+		echo 'お客様情報を更新しました。';
+	} else {
+		$sql=$pdo->prepare('insert into customer values(null,?,?,?,?,?)');
+		$sql->execute([
+			$_REQUEST['name'], $_REQUEST['address'], 
+			$_REQUEST['login'], $_REQUEST['password'], $_REQUEST['mailaddress']]);
+		echo 'お客様情報を登録しました。';
+	}
+} else {
+	echo 'ログイン名がすでに使用されていますので、変更してください。';
+}
+?>
+<?php require '../footer.php'; ?>
